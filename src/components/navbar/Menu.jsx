c445e6fee8ada541525e29c8ddbf54eeb885a0b9
Juan Jose <chevike15@gmail.com>
2023-09-14 15:05:06 -0600

    Haciendo la barra de navegacion

---
+++ src/components/navbar/Menu.jsx
@@ -0,0 +1,13 @@
+const Menu = () => {
+  return (
+    <>
+      <a href="#inicio">Inicio</a>
+      <a href="#que-es">Que es GPT?</a>
+      <a href="#possibility">Open IA</a>
+      <a href="#features">Casos de Estudio</a>
+      <a href="#blog">Biblioteca</a>
+    </>
+  );
+};
+
+export default Menu;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,60 @@
+import { useState } from 'react';
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
+import logo from '../../assets/logo4.svg';
+import Menu from './Menu';
 import './navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // variable de estado
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links-logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="navbar-links-container">
+          <Menu />
+        </div>
+      </div>
+
+      <div className="navbar-sign">
+        <a>Iniciar Sesion</a>
+        <button>Registrate</button>
+      </div>
+
+      {/* menu para movil */}
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+
+        {toggleMenu && (
+          <div className="navbar-menu-container scale-up-center">
+            <div className="navbar-menu-container-links">
+              <Menu />
+              <div className="navbar-menu-container-sign">
+                <a>Iniciar Sesion</a>
+                <button>Registrate</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
